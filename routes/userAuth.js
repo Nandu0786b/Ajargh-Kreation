9927bad9de6e842b44b71ed481ce5f69a4ff5015
Nand Kishore Ola <nandkishoreola0786b@gmail.com>
2023-10-23 23:32:08 +0530

    user routes to register new user

---
+++ routes/userAuth.js
@@ -0,0 +1,13 @@
+import express from "express";
+import * as auth from "../controllers/auth.js";
+import jwtToken from "../middleware/Jwt.js";
+import * as rateLimit from "../middleware/rateLimiter.js";
+
+const router = express.Router();
+
+// Post routes
+router.post("/register",rateLimit.ipLimit, auth.register);
+
+
+
+export default router;
