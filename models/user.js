984821b7165bf7516f0fa631235e15256d0d6741
Nand Kishore Ola <nandkishoreola0786b@gmail.com>
2023-10-23 23:27:38 +0530

    Mongo Db model for the user details

---
+++ models/user.js
@@ -0,0 +1,31 @@
+import mongoose  from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name :{
+        type : String,
+        max:25,
+        min:2,
+        require:true
+    },
+    email:{
+        type:String,
+        max:50,
+        min:4,
+        require:true,
+        unique:true
+    },
+    pass:{
+        type:String,
+        max:20,
+        min:7
+    }
+
+},
+{
+    timestamps: true,
+    versionKey: '__v', // Enable versioning
+})
+
+mongoose.pluralize(null);
+const user = mongoose.model("User", userSchema);
+export default user;
